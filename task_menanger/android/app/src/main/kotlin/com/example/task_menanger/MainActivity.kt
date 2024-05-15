e43c1bb73e271e5e5e698f2288a1861cb6ff9ec1
Blaž Perić <blaz.peric4@gmail.com>
2024-05-15 23:43:35 +0200

    remove unnececery files

--- task_menanger/android/app/src/main/kotlin/com/example/task_menanger/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.task_menanger
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- task_menanger/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:task_menanger/app.dart';
-
-void main() {
-  runApp(const Application());
-}
