ffa8ad565fcd2484bdfd5b1227a60d06b119447e
Blaž Perić <blaz.peric4@gmail.com>
2024-05-23 09:30:10 +0200

    povezivanje sa APIjem

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+
 import 'package:task_menanger/app.dart';
 
 void main() {
--- lib/task_model.dart
+++ lib/task_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class TaskModel {
   final String title;
@@ -8,4 +10,49 @@ class TaskModel {
     required this.description,
     required this.isActive,
   });
+
+  TaskModel copyWith({
+    String? title,
+    String? description,
+    bool? isActive,
+  }) {
+    return TaskModel(
+      title: title ?? this.title,
+      description: description ?? this.description,
+      isActive: isActive ?? this.isActive,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'description': description,
+      'isActive': isActive,
+    };
+  }
+
+  factory TaskModel.fromMap(Map<String, dynamic> map) {
+    return TaskModel(
+      title: map['title'] as String,
+      description: map['description'] as String,
+      isActive: map['isActive'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory TaskModel.fromJson(String source) => TaskModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'TaskModel(title: $title, description: $description, isActive: $isActive)';
+
+  @override
+  bool operator ==(covariant TaskModel other) {
+    if (identical(this, other)) return true;
+
+    return other.title == title && other.description == description && other.isActive == isActive;
+  }
+
+  @override
+  int get hashCode => title.hashCode ^ description.hashCode ^ isActive.hashCode;
 }
